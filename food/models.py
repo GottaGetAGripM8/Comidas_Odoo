7b950b42402374e439652e752f7f45e5d50dc71a
Javier Pardos <jpbyepb@gmail.com>
2017-03-05 16:42:12 +0100

    Añadidos nuevos cambios: mas campos, mas vistas ... (Sujeto a cambios)
    Signed-off-by: Javier Pardos <jpbyepb@gmail.com>

--- food/models.py
+++ food/models.py
@@ -6,16 +6,17 @@ class Platos(models.Model):
 	_name = 'food.platos'
 
 	name = fields.Char(string="Nombre del Plato", required=True)
+	tipo = fields.Selection([('primer_plato','Primer Plato'), ('segundo_plato','Segundo Plato'), ('postre','Postre')], 'tipo_de_plato')
 
-	#camareros_id = fields.Many2many(string="Camareros", comodel_name='zoo.camareros', relation='rel_camarero_plato', column1='plato', column2='camarero')
-	#veterinarios_id=fields.Many2many(string="Veterinarios", comodel_name='zoo.veterinario', relation='rel_veterinario_animal', column1='animal', column2='veterinario')s
+	camareros_id = fields.Many2many(string="Camareros", comodel_name='food.camareros', relation='rel_camarero_plato', column1='plato', column2='camarero')
 
-#class Decoracion(models.Model): #1 decoracion puede estar en N varias mesas
-	#_name = 'food.decoracion'
+class Decoracion(models.Model): 
+	_name = 'food.decoracion'
 
-	#name = fields.Char(string="Nombre de la decoracion", required=True)
+	name = fields.Char(string="Nombre de la decoracion", required=True)
+	tipo = fields.Selection([('invernal','Festival de Invernal'), ('halloween','Festival de Halloween'), ('primavera','Festival de la Primavera')], 'decoracion_festival')
 
-	#decoracion_id = fields.One2many('food.decoracion', 'mesas_id', string="Decoracion")
+	decoracion_id = fields.One2many('food.decoracion', 'mesas_id', string="Decoracion")
 
 
 class Camareros(models.Model): 
@@ -23,12 +24,13 @@ class Camareros(models.Model):
 
 	name = fields.Char(string="Nombre del camarero", required=True)
 	sexo = fields.Selection([('mujer','Mujer'), ('hombre','Hombre')], 'sexo')
+	edad = fields.Integer(string="Edad ", required=True)
 
 	mesas_id = fields.One2many('food.camareros', 'mesas_id', string="Mesa")
 	platos_id = fields.Many2many(string="Platos", comodel_name='food.platos', relation='rel_plato_camareros', column1='camareros', column2='plato')
-	#animales_id=fields.Many2many(string="Animales", comodel_name='zoo.animal', relation='rel_animal_veterinario', column1='veterinario', column2='animal')
 	
-class Mesas(models.Model): # 1 MESA solo la lleva un CAMARERO
+	
+class Mesas(models.Model):
 	_name = 'food.mesas'
 
 	name = fields.Char(string="Nombre de la mesa", required=True)
